0245822884adb6eab1730212a5d6587bf4669959
Syahir Zakwan <syahirzakwan.dev@gmail.com>
2022-09-12 22:30:33 +0800

    Update footer

--- digital-business-card/src/components/Footer.jsx
+++ digital-business-card/src/components/Footer.jsx
@@ -1,9 +1,9 @@
 function Footer() {
-  return(
+  return (
     <div>
-      <small>&copy; {new Date().getFullYear()} syhrzkwn.dev. All Right Reserved.</small>
+      <small>&copy; {new Date().getFullYear()} syhrzkwn.dev | All Right Reserved.</small>
     </div>
-  )
+  );
 }
 
-export default Footer
+export default Footer;
